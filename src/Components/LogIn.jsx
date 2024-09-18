72141f40631d1cd48522c4332c697fcbbe01a012
Rakesh Patel <rakesh246808@gmail.com>
2024-09-18 22:49:46 +0530

    updated

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -3,20 +3,22 @@ function LogIn()
     return (
        <>
          <div className="user-details">
-                    <div className="input-containers">
-                        <input type="text" placeholder="Enter Your Name" required/>
-                    </div>
-                    <div className="input-containers">
-                        <input type="text" placeholder="Enter Your Name" required/>
-                    </div>
+                <div className="input-containers">
+                    <input type="text" placeholder="Enter Your First Name" required/>
                 </div>
+                <div className="input-containers">
+                    <input type="text" placeholder="Enter Your Last Name" required/>
+                </div>
+         </div>
+         <div className="user-details">
                 <div className="input-containers">
                     <input type="email" placeholder="Email" required/>
                 </div>
                 <div className="input-containers">
                     <input type="password" placeholder="Password" required/>
                 </div>
-                <button className="subBtn" type="submit">Log In</button>
+         </div>       
+         <button className="subBtn" type="submit">Log In</button>
        </>
     )
 }
