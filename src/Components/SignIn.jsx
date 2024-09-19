e95674362ddf53dcc9ca4df00acfcae176696f78
Rakesh Patel <rakesh246808@gmail.com>
2024-09-19 22:27:59 +0530

    updated

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -4,20 +4,21 @@ function SignIn(props)
     return (
        <>
          <div className="user-details">
-                    <div className="input-containers">
-                        <input type="text" placeholder="Enter Your Name" required/>
-                    </div>
-                    <div className="input-containers">
-                        <input type="text" placeholder="Enter Your Name" required/>
-                    </div>
+                <div className="input-containers">
+                    <input type="text" placeholder="Enter Your First Name" required/>
                 </div>
+                <div className="input-containers">
+                    <input type="text" placeholder="Enter Your Last Name" required/>
+                </div>
+         </div>
+         <div className="user-details">
                 <div className="input-containers">
                     <input type="email" placeholder="Email" required/>
                 </div>
                 <div className="input-containers">
                     <input type="password" placeholder="Password" required/>
                 </div>
-
+         </div>
                 <div className="input-containers">
                     <input type="password" placeholder="Confirm Password" required/>
                 </div>
